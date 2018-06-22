332e651f604f7b684becc36343ec54d48098c202
Igor Orlov <igor39456@gmail.com>
2018-06-23 04:14:37 +0800

    Revert "HelloWorld Application"
    This reverts commit 1f0c54e1599fcf2dfd0a4c538791e4113d10739a.

--- HelloWorld/urls.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import TemplateView
-from django.urls import include, path, re_path
-from . import views
-
-urlpatterns = [
-    path('hello/', views.hello, name = 'hello'),
-
-]
